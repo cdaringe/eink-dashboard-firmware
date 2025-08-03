b4a8c89f1906c4b4cb61acd869f30016d6e958b1
cdaringe <cdaringe@gmail.com>
2025-08-02 18:15:06 -0700

    fix: default uri

--- eink-dashboard-firmware/inkconfig.cpp
+++ eink-dashboard-firmware/inkconfig.cpp
@@ -25,7 +25,8 @@ int get_estimated_remaining_battery_percentage(double voltage)
 
 void write_dashboard_uri_string(Inkplate &display, const char* dashboard_uri, char* uri512buff)
 {
-  if (dashboard_uri[0] == '/') {
+  bool is_dashboard_slug_load = dashboard_uri[0] == "" || dashboard_uri[0] == '/';
+  if (is_dashboard_slug_load) {
     double voltage = display.readBattery();
     int remaining_percent = get_estimated_remaining_battery_percentage(voltage);
     snprintf(uri512buff, 512, "%s%s?textoverlay=%.1fV,498,1175,16&batteryoverlay=%d,540,1172,x24", DASHBOARD_BASE_URL, dashboard_uri, voltage, remaining_percent);
